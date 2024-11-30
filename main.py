09f3426418e0fa824e65ed1b47f7ec9875f31d06
hipoloco <rodrigofernandezfranco@gmail.com>
2024-11-30 15:40:19 -0300

    Documentación actualizada en main.py

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-import signal, sys
+"""
+main.py
+
+Este módulo sirve como punto de entrada para la aplicación. Proporciona un menú principal 
+que permite al usuario seleccionar entre diferentes funcionalidades: analizar contraseñas, 
+generar contraseñas, o hashear contraseñas. También maneja la interacción del usuario y 
+la navegación entre las opciones de manera controlada y segura.
+"""
+
+import sys
 from getpass import getpass
 
 import checkpass, passgenerator, hashpass
 from modules.constants import APP_NAME, APP_VER
-from modules.utils import clear_console, cprint, handle_program_exit
-
-#signal.signal(signal.SIGINT, signal_handler_exit)
+from modules.utils import cprint, handle_program_exit, show_header
 
 def mostrar_menu():
-    clear_console()
-    cprint(f"=== {APP_NAME} v{APP_VER} - MENÚ PRINCIPAL ===\n", "Y")
+    """
+    Muestra el menú principal de la aplicación y solicita al usuario seleccionar una opción.
+
+    Returns:
+        str: Opción seleccionada por el usuario.
+    """
+
+    show_header(f"=== {APP_NAME} v{APP_VER} - MENÚ PRINCIPAL ===\n")
     print("1. Analizar contraseña")
     print("2. Generar contraseña")
     print("3. Hashear contraseña")
@@ -19,21 +32,35 @@ def mostrar_menu():
     return opcion
 
 def menu():
+    """
+    Controla la navegación en el menú principal de la aplicación.
+
+    Permite al usuario seleccionar una de las funcionalidades disponibles:
+    - Analizar contraseña (opción 1)
+    - Generar contraseña (opción 2)
+    - Hashear contraseña (opción 3)
+    - Salir del programa (opción 4)
+
+    Maneja entradas inválidas y permite reintentar la selección. También
+    maneja la interrupción por teclado (Ctrl+C) para una salida controlada.
+    """
+    
     while True:
         opcion = mostrar_menu()
 
         if opcion == "1":
-            checkpass.checkpass()
+            checkpass.checkpass()  # Llama al módulo de análisis de contraseñas
         elif opcion == "2":
-            passgenerator.passgenerator()
+            passgenerator.passgenerator()  # Llama al módulo de generación de contraseñas
         elif opcion == "3":
-            hashpass.hashpass()
+            hashpass.hashpass()  # Llama al módulo de hashing de contraseñas
         elif opcion == "4":
             cprint(f"\n[*] Saliendo de {APP_NAME}.\n", "B")
-            sys.exit(0)
+            sys.exit(0)  # Finaliza el programa
         else:
-            getpass("\nOpción incorrecta, presione ENTER para continuar.")
+            getpass("\nOpción incorrecta, presione ENTER para continuar.")  # Maneja opciones inválidas
 
+# Bloque principal para iniciar el programa y manejar interrupciones controladas
 try:
     menu()
 except KeyboardInterrupt:
